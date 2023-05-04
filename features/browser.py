fdbe59f0ce7ca01845081033c9b67587b32dc142
gokulan-devs <87415855+gokulan-devs@users.noreply.github.com>
2023-05-04 22:53:47 +0530

    Update browser.py

--- features/browser.py
+++ features/browser.py
@@ -1,5 +1,4 @@
 my_file = (os.path.join(os.getcwd(), 'setup.cfg'))
-    config.read(my_file)
- 
-    # Reading the browser type from the configuration file for Selenium test automation
-    helper_func = get_browser(config.get('Environment', 'Browser'))
+config.read(my_file)
+# Reading the browser type from the configuration file for Selenium test automation
+helper_func = get_browser(config.get('Environment', 'Browser'))
