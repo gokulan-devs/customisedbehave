2023529d21e28e20e57ad3ab7ea909fa6c67293f
gokulan-devs <87415855+gokulan-devs@users.noreply.github.com>
2023-05-05 02:00:06 +0530

    Update locators.py

--- features/locators.py
+++ features/locators.py
@@ -1,6 +1,6 @@
 import os
 import yaml
 
-locator_file= (os.path.join(os.getcwd(), '/features/locators.yaml'))
+locator_file= locators.yaml
 locators =yaml.load(open(locator_file),Loader=yaml.FullLoader)
     
