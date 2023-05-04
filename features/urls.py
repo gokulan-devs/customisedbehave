79e5da2f678113c252e5460f134d988f1487ce4d
gokulan-devs <87415855+gokulan-devs@users.noreply.github.com>
2023-05-05 02:12:38 +0530

    Update urls.py

--- features/urls.py
+++ features/urls.py
@@ -1,6 +1,6 @@
 import os
 import yaml
 
-urls_file= (os.path.join(os.getcwd(), 'urls.yaml'))
+urls_file= (os.path.join(os.getcwd(), 'features/urls.yaml'))
 urls =yaml.load(open(urls_file),Loader=yaml.FullLoader)
     
